5605cac61f52f8bde70f4f56ad25ed85b88653db
user0000000000000000 <143264596+user0000000000000000@users.noreply.github.com>
2023-10-20 12:09:04 +0200

    Update 1#petalos.py

--- 1#petalos.py
+++ 1#petalos.py
@@ -1,31 +1,33 @@
 import turtle
 import random
 
-def halfPetal(sz, ang):
-    jem.circle(sz,ang)
-    jem.circle(sz,ang)
+def halfPetal(t, sz, ang):
+    t.circle(sz,ang)
+    t.circle(sz,ang)
 
-def Petal(sz, ang):
-    halfPetal(sz, ang)
-    jem.left(40)
-    halfPetal(sz, ang)
+def Petal(t, sz, ang):
+    halfPetal(t, sz, ang)
+    t.left(40)
+    halfPetal(t, sz, ang)
 
-wn = turtle.Screen()
-jem = turtle.Turtle()
+def main():
+    wn = turtle.Screen()
+    jem = turtle.Turtle()
 
-jem.color("yellow")
-fill_color = "yellow"
-jem.pensize(2)
+    jem.color("yellow")
+    jem.pensize(2)
 
-t = int(input("Number of petals?" ))
+    t = int(input("Number of petals?" ))
 
-for _ in range(t):
-    jem.penup()
-    jem.goto(int(random.randrange(-300,300)), int(random.randrange(-300,300)))
-    jem.begin_fill()
-    jem.pendown()
-    Petal(100,70)
-    jem.end_fill()
-    jem.penup()
+    for _ in range(t):
+        jem.penup()
+        jem.goto(int(random.randrange(-300,300)), int(random.randrange(-300,300)))
+        jem.begin_fill()
+        jem.pendown()
+        Petal(jem, 100,70)
+        jem.end_fill()
+        jem.penup()
 
-wn.exitonclick()
+    wn.exitonclick()
+
+main()
