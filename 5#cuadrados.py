0e228cff99192ad518388056947709827fb4c3f3
user0000000000000000 <143264596+user0000000000000000@users.noreply.github.com>
2023-10-20 11:24:36 +0200

    Update 5#cuadrados.py

--- 5#cuadrados.py
+++ 5#cuadrados.py
@@ -1,17 +1,17 @@
 import turtle
 
+def SQUARE(sz):
+    """Draw square"""
+    for _ in range(4):
+        w.forward(sz)
+        w.left(90)
+        
 wn = turtle.Screen()
 w = turtle.Turtle()
 
 squares = int(input("Number of squares?: " ))
 lado = int(input("Side length?: " ))
 ang = int(input("ANgle between squares?: " ))
-
-def SQUARE(sz):
-    """Draw square"""
-    for _ in range(4):
-        w.forward(sz)
-        w.left(90)
 
 for _ in range(squares):
     SQUARE(lado)
