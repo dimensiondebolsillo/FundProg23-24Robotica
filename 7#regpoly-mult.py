655c942cb556d096e1f021c91256fb4250d6c230
user0000000000000000 <143264596+user0000000000000000@users.noreply.github.com>
2023-10-20 11:26:46 +0200

    Update 7#regpoly-mult.py

--- 7#regpoly-mult.py
+++ 7#regpoly-mult.py
@@ -1,12 +1,5 @@
 import turtle
 
-wn = turtle.Screen()
-
-w = turtle.Turtle()
-
-l = int(input("Side length?" ))
-alpha = int(input("Degrees between polygons? "))
-r = int(360 / alpha)
 def drawPoly(t, somesides, somesize):
     """Get turtle t to draw a regular polygon X side and Y size"""
 
@@ -14,6 +7,12 @@ def drawPoly(t, somesides, somesize):
         t.forward(somesize)
         t.left(360/somesides)
 
+wn = turtle.Screen()
+w = turtle.Turtle()
+
+l = int(input("Side length?" ))
+alpha = int(input("Degrees between polygons? "))
+r = int(360 / alpha)
 
 for _ in range(r):
     drawPoly(w, r, l)
